0483359fe261a6c31433d5c7fe980b2adf72e40c
Barjuan Davis <barjuandavis@gmail.com>
2024-02-08 16:22:17 +0700

    added more routing stuff

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,8 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import {
+  createRootRoute,
+  //Link,
+  Outlet,
+} from "@tanstack/react-router";
 //import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 //import { Suspense } from "react";
 
@@ -8,15 +12,14 @@ export const Route = createRootRoute({
       {/* <Suspense>
         {process.env.NODE_ENV === "development" && <TanStackRouterDevtools />}
       </Suspense> */}
-      <div className="p-2 flex gap-2">
+      {/* <div className="p-2 flex gap-2">
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{" "}
         <Link to="/about" className="[&.active]:font-bold">
           About
         </Link>
-      </div>
-      <hr />
+      </div> */}
       <Outlet />
     </>
   ),
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,16 +10,36 @@ import MousepadAbout from "@/components/about/MousepadAbout";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import NewMousepadList from "@/components/NewMousepadList";
+import { z } from "zod";
+import {
+  Link,
+  createFileRoute,
+  useNavigate,
+  useSearch,
+} from "@tanstack/react-router";
 
-import { createLazyFileRoute } from "@tanstack/react-router";
+const searchSchema = z.object({
+  itemType: z.enum(["mouse", "mousepad"]).catch("mouse"),
+});
 
-export const Route = createLazyFileRoute("/")({
+//type Search = z.infer<typeof searchSchema>
+
+export const Route = createFileRoute("/")({
   component: Index,
+  validateSearch: searchSchema,
 });
 
 function Index() {
+  const { itemType } = useSearch({
+    from: Route.fullPath,
+  });
+  const navigate = useNavigate({
+    from: Route.fullPath,
+  });
+
   useEffect(() => {
     posthog.capture("Homepage Viewed");
+    console.log("itemType", itemType);
   }, []);
 
   type ContentType = "mouse" | "mousepad";
@@ -28,14 +48,14 @@ function Index() {
     ContentType | null | undefined
   >(undefined);
   const [variousLinksOpened, setVariousLinksOpened] = useState(false);
-  const [contentType, setContentType] = useState<ContentType>("mouse");
+
+  //convert to usestate to search params using tanstack router
 
   const setAboutSectionBasedOnContentType = (open: boolean) => {
-    if (contentType === "mouse") {
-      setAboutOpened(open ? "mouse" : undefined);
-    } else {
-      setAboutOpened(open ? "mousepad" : undefined);
-    }
+    posthog.capture("About Section Toggled", {
+      itemType,
+    });
+    setAboutOpened(open ? itemType : null);
   };
 
   return (
@@ -61,22 +81,33 @@ function Index() {
             style it using Tailwind
             */}
             <VariousLinksSection opened={variousLinksOpened} />
-            <Tabs defaultValue="mouse" className="w-full">
+            <Tabs defaultValue={itemType} className="w-full">
               <TabsList className="py-2 bg-slate-300">
                 <TabsTrigger
                   className="px-8"
                   value="mouse"
                   onClick={() => {
-                    setContentType("mouse");
+                    posthog.capture("Tab Clicked", {
+                      tab: "Mouse",
+                    });
+                    navigate({
+                      search: () => ({ itemType: "mouse" }),
+                    });
                   }}
                 >
                   Mouse
                 </TabsTrigger>
+
                 <TabsTrigger
                   value="mousepad"
                   className="px-8"
                   onClick={() => {
-                    setContentType("mousepad");
+                    posthog.capture("Tab Clicked", {
+                      tab: "Mousepad",
+                    });
+                    navigate({
+                      search: () => ({ itemType: "mousepad" }),
+                    });
                   }}
                 >
                   Mousepad
